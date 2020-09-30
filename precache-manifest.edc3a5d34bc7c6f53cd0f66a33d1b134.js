97b9ea1e04a479ba8fa6ffc3f974686db7ad28d7
lsw6684 <lsw6684@gmail.com>
2020-09-30 15:46:13 +0900

    Updates

---
+++ precache-manifest.edc3a5d34bc7c6f53cd0f66a33d1b134.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e3da100a45392fa136b9722e4741936",
+    "url": "/NetfliXeung_MovieApp/index.html"
+  },
+  {
+    "revision": "c79d9917b51d8c2ea195",
+    "url": "/NetfliXeung_MovieApp/static/css/main.a5603444.chunk.css"
+  },
+  {
+    "revision": "2cb00f0b337cb1b1485d",
+    "url": "/NetfliXeung_MovieApp/static/js/2.03729dac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/NetfliXeung_MovieApp/static/js/2.03729dac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c79d9917b51d8c2ea195",
+    "url": "/NetfliXeung_MovieApp/static/js/main.53808796.chunk.js"
+  },
+  {
+    "revision": "a253843e932327070ece",
+    "url": "/NetfliXeung_MovieApp/static/js/runtime-main.5d9d63e7.js"
+  }
+]);
